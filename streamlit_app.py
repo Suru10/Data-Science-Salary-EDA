1462e25b7a9b309f85be38398587edfe3031b2cd
Suraj Patel <surajpatel8300@gmail.com>
2023-06-30 13:23:20 -0400

    Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,13 @@ import warnings
 import seaborn as sns
 import plotly.express as px
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 
 warnings.filterwarnings("ignore")
 df = pd.read_csv('ds_salaries.csv')
 # Introduction
 st.header('Whimsical Wolves ')
 st.write(
-  ' Hi, my name is Jamie, I am in 7th grade and I have learned how to code in the past 5 days'
+  ' Hi, my name is Jamie, I am in 8th grade and I have learned how to code in the past 5 days'
 )
 st.write(
   "Hello, my name is Joshua, I am in 10th grade and I've been coding for the last 4 years. I've learned languages such as python, java, and SQL."
@@ -30,21 +29,35 @@ st.header('Inspection:')
 st.write(" This is an example of what our data looks like:")
 st.write("\n")
 st.write(df.head())
-
-st.write(
-  'The experience level and The employee residence columns have symbols that could be unclear or confusing, so it will be fixed later.'
-)
+st.write("\n")
+st.write('Null Values')
 
 col1, col2 = st.columns(2)
 col1.write(df.isna().sum())
 col2.write(
-  "According to the graph there is no null values, that's good because it keeps the data consistent."
+  "According to the table there is no null values, that's good because it keeps the data consistent."
 )
 st.markdown("""---""")
-# Code for Cleaning:
-#Drop Code
+st.header('Cleaning')
+st.write("\n")
+
+st.write('DROPPING COLUMNS')
+col1, col2 = st.columns(2)
 columns_to_drop = ['salary', 'salary_currency', 'employment_type']
 df.drop(columns_to_drop, axis=1, inplace=True)
+col1.write(df.head())
+st.write("\n")
+col2.subheader('Reasoning')
+col2.write(
+  " The salary, salary currency, were dropped because they were useless becuase the data already had salary in usd. employment type was dropped because we didn't need it."
+)
+st.write(
+  'The experience level and The employee residence columns have symbols that could be unclear or confusing, so it will be fixed later.'
+)
+
+# Code for Cleaning:
+#Drop Code
+
 #Value Name Change
 df['experience_level'] = df['experience_level'].replace(
   'SE', 'Senior Executive')
@@ -154,12 +167,10 @@ st.plotly_chart(fig, use_container_width=True)
 st.write(
   'This bar graph shows the average salary of a data scientists based off their job title.'
 )
-st.markdown("""---""")
+st.markdown("""""")
 st.write("Salaries range from as low as 5,400$ per year to as high as 375,000")
-st.markdown("""---""")
-st.write(
-  "The highest paying job titles on average are Data Science Tech lead, Cloud Data Architect, and Data Lead"
-)
+st.markdown("""""")
+
 # Summary of graph
 
 # This bar graph shows the average salary of a data scientists based off their job title. This graph is very straghtforward and it is clear that there is a big wage gap between job titles.
@@ -168,7 +179,7 @@ st.write(
 
 # Insert code here
 st.subheader(
-  "Hypthesis 2: How much does a data scientist's experience level affect their salary?"
+  "Hypothesis 2: On average, which experience level, for data scientists, has the highest salary?"
 )
 
 fig, ax = plt.subplots()
@@ -191,9 +202,9 @@ st.pyplot(fig)
 st.write(
   "This graph shows the distribution of salaries based off experience level. The thicker the violin graph gets the more people are in that salary range."
 )
-st.markdown("""---""")
+st.markdown("""""")
 st.write(
-  "There is a clear gap between experience levels with entry level data scientists only averaging 75k per year while senior executives average almost 200k per year."
+  "In order from the lowest average salary to the highest average salary there was on average a 35.5% increase per level. This shows that experience level plays a major factor in determining a data scientists average salary."
 )
 
 # The thicker the violin graph gets the more people are in that salary range.
@@ -203,7 +214,7 @@ st.write(
 #Hypothesis 3: How does the country of residence affect a data scientist’s salary?-Jamie
 # Insert code here
 st.subheader(
-  "Hypthesis 3: How does the country of residence affect a data scientist’s salary?"
+  "Hypothesis 3: How does the country of residence affect a data scientist’s salary?"
 )
 Hypothesis_3 = df.groupby(
   "employee_residence")['salary_in_usd'].mean().reset_index()
@@ -218,7 +229,7 @@ st.plotly_chart(fig, use_container_width=True)
 st.write(
   "Overall, most of the countries make the same amount of data scientists, although, the countries with more data scientist generally make more than the countiries with less data scientist."
 )
-st.markdown("""---""")
+st.markdown("""""")
 st.write(
   "The only outlier data point is israel, they make over 400k, with only having one Data scientist."
 )
@@ -239,10 +250,28 @@ st.plotly_chart(fig, use_container_width=True)
 st.write(
   "The information from the graph above shows that data scientists who do none of their work remote tend to make significantly more than data scientists who do 50% remote work."
 )
-st.markdown("""---""")
+st.markdown("""""")
 st.write(
   "Data scientists who only do remote work actually average a salary of 135k per year which is significantly more than data scientists with 50% remote work but still less than data scientists with zero remote work."
 )
 st.markdown("""---""")
 st.title("Final Conclusions")
-st.subheader("Hypothesis 1")
+st.markdown("""---""")
+st.subheader(
+  "Hypothesis 1: Which job titles, for data science, on average have the highest paying salaries?"
+)
+st.write(
+  "The highest paying job titles on average are Data Science Tech lead, Cloud Data Architect, and Data Lead"
+)
+st.markdown("""---""")
+st.subheader(
+  "Hypothesis 2: On average, which experience level, for data scientists, has the highest salary?"
+)
+st.write(
+  "According to the second graph, Executive level data scientists tend to have the highest average salary at 194,930$ per year."
+)
+st.markdown("""---""")
+st.subheader(
+  "Hypothesis 3: How does the country of residence affect a data scientist’s salary?"
+)
+st.write("")
