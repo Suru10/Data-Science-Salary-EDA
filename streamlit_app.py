160cf45996c278785c4442e582d859012f751644
Suraj Patel <surajpatel8300@gmail.com>
2023-06-30 14:09:35 -0400

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,21 +26,22 @@ st.markdown("""---""")
 # Inspection:
 #exampple of the data
 st.header('Inspection:')
-st.write(" This is an example of what our data looks like:")
+st.write(" THIS IS AN EXAMPLE OF WHAT OUR DATA LOOKS LIKE:")
 st.write("\n")
 st.write(df.head())
 st.write("\n")
-st.write('Null Values')
+st.write('\n NULL VALUES')
 
 col1, col2 = st.columns(2)
 col1.write(df.isna().sum())
-col2.write(
-  "According to the table there is no null values, that's good because it keeps the data consistent."
+col2.subheader(
+  "According to the table there is no null values, it keeps the data consistent."
 )
 st.markdown("""---""")
+# Cleaning:
 st.header('Cleaning')
 st.write("\n")
-
+#Dropping columns
 st.write('DROPPING COLUMNS')
 col1, col2 = st.columns(2)
 columns_to_drop = ['salary', 'salary_currency', 'employment_type']
@@ -49,15 +50,25 @@ col1.write(df.head())
 st.write("\n")
 col2.subheader('Reasoning')
 col2.write(
-  " The salary, salary currency, were dropped because they were useless becuase the data already had salary in usd. employment type was dropped because we didn't need it."
+  " The salary and salary currency columns were dropped because they were useless becuase the data already had salary in usd. The employment type column was dropped because we didn't need it."
 )
-st.write(
-  'The experience level and The employee residence columns have symbols that could be unclear or confusing, so it will be fixed later.'
-)
+st.write("\n")
+st.subheader('Changing the symbols')
+col1, col2 = st.columns(2)
+col1.write('BEFORE THE CHANGE')
+data = {
+  'experience_level': ['SE', 'MI', 'EN', 'EX'],
+  'employee_residence': [
+    'ES',
+    'US',
+    'CA',
+    'DE',
+  ]
+}
 
-# Code for Cleaning:
-#Drop Code
-
+df = pd.DataFrame(data)
+col1.table(df)
+col2.write("AFTER THE CHANGE")
 #Value Name Change
 df['experience_level'] = df['experience_level'].replace(
   'SE', 'Senior Executive')
@@ -147,6 +158,23 @@ country_mapping = {
   'MT': 'Malta'
 }
 df['employee_residence'] = df['employee_residence'].replace(country_mapping)
+data = {
+  'experience_level':
+  ['Senior Executive', 'Middle Level', 'Entry Level', 'Executive'],
+  'employee_residence': [
+    'Spain',
+    'United States',
+    'Canada',
+    'Germany',
+  ]
+}
+
+df = pd.DataFrame(data)
+col2.table(df)
+st.subheader('Reasoning')
+st.write(
+  "The experience level and The employee residence columns have symbols that could be unclear or confusing, so it was changed to it's orignal meaning."
+)
 
 # Visualizations:
 
@@ -237,7 +265,7 @@ st.write(
 # Hypothesis 4: How does the amount of remote work affect a data science  salary in usd? -Jamie
 # Insert code here
 st.subheader(
-  "Hypthesis 4: Does the amount of remote work a data scientists has affect their average salary?"
+  "Hypothesis 4: Does the amount of remote work a data scientists has affect their average salary?"
 )
 Hypothesis_4 = df.groupby("remote_ratio")['salary_in_usd'].mean().reset_index()
 fig = px.pie(Hypothesis_4,
@@ -250,10 +278,7 @@ st.plotly_chart(fig, use_container_width=True)
 st.write(
   "The information from the graph above shows that data scientists who do none of their work remote tend to make significantly more than data scientists who do 50% remote work."
 )
-st.markdown("""""")
-st.write(
-  "Data scientists who only do remote work actually average a salary of 135k per year which is significantly more than data scientists with 50% remote work but still less than data scientists with zero remote work."
-)
+
 st.markdown("""---""")
 st.title("Final Conclusions")
 st.markdown("""---""")
@@ -274,4 +299,13 @@ st.markdown("""---""")
 st.subheader(
   "Hypothesis 3: How does the country of residence affect a data scientist’s salary?"
 )
-st.write("")
+st.write(
+  "A data scientists country of residence greatly affects what their salary may look like. Countries such as Indonesia or Ghana have an average data scientist salary of around 15k USD per year while countries such as the US and Canada have an average data scientist salary around 150k USD per year salary."
+)
+st.markdown("""---""")
+st.subheader(
+  "Hypothesis 4: Does the amount of remote work a data scientists has affect their average salary?"
+)
+st.write(
+  "Data scientists who only do remote work actually average a salary of 135k per year which is significantly more than data scientists with 50% remote work but still less than data scientists with zero remote work."
+)
